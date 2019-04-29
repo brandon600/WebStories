8e858fb0747e59e74a507dc957cb6f87930e871a
Brandon Rodriguez <brandonart1224@gmail.com>
2019-04-28 20:42:05 -0700

    Initial Commit To Deploy

---
+++ config/passport.js
@@ -0,0 +1,56 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('./keys');
+
+//Load User Model
+const User = mongoose.model('users');
+
+module.exports = function(passport) {
+    passport.use(
+        new GoogleStrategy({
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+            proxy: true
+        }, (accessToken, refreshToken, profile, done) => {
+               // console.log(accessToken);
+                 console.log(profile);
+
+                 const image = profile.photos[0].value
+
+                 console.log(`The image is ${image} and that is that.`);
+
+                 const newUser = {
+                     googleID: profile.id,
+                     firstName: profile.name.givenName,
+                     lastName: profile.name.familyName,
+                     email: profile.emails[0].value,
+                     image: image
+                 }
+
+                 //Check for existing user
+                 User.findOne({
+                     googleID: profile.id
+                 }).then(user => {
+                     if (user) {
+                        done(null, user);
+                        console.log('user has already been added');
+                     } else {
+                         //Create User
+                         new User(newUser).save()
+                         .then(user => done(null, user));
+                         console.log('New user has been added');
+                     }
+                 })
+
+        })
+    );
+
+    passport.serializeUser((user, done) => {
+       done(null, user.id); 
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id).then(user => done(null, user));
+    });
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1>Web Stories</h1>
+
+<p>This is a project I made utilizing Express, Node, & MongoDB that allows users to share stories about their web development careers. They can make their stories public, or make them private for their eyes only.</p>
